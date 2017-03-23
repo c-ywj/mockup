503a771f23bcdb1db4e7fc1c05756fe550832a69
Lucy Wang <LucyTheGreat@Lucys-MacBook-Air.local>
2017-03-23 15:12:56 -0400

    basic searchbars

---
+++ mockPro/api/controllers/ProductController.js
@@ -0,0 +1,18 @@
+/**
+ * ProductController
+ *
+ * @description :: Server-side logic for managing products
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	//var http = require("http"), options = {
+	// host: 'HTTP'
+	//port: 80
+	//path: //
+	//method: 'GET'
+//}
+	// require Montecarlo algo and pick 2 from top 5 items
+
+};
---
+++ mockPro/api/models/Product.js
@@ -0,0 +1,15 @@
+/**
+ * Product.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    //product: validate top 5 items from amazon API
+    //validates the 2 brands
+
+  }
+};
